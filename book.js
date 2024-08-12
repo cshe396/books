5355f6f74dc50c2ea29cd60113f0ff44ffefe31d
Cj <cjsherrow@gmail.com>
2024-08-12 18:22:26 -0500

    Add modal with form

--- book.js
+++ book.js
@@ -2,43 +2,70 @@ let book1 = new Book("We were soldiers Once...and Young", 389, 'Harold G. Moore'
 let book2 = new Book("The Indifferent Stars Above: The harrowing saga of the donner party", 400, 'Daniel James Brown', false);
 const myLibrary = [book1, book2];
 const libraryTableRow = document.getElementById('third-row');
-
+const addBookBtn = document.getElementById('add-book');
+const favDialog = document.getElementById('favDialog');
 displayBooks();
 
-function Book(title, page_count, author, is_read){
+addBookBtn.addEventListener("click", () => {
+    favDialog.showModal();
+    console.log("henlo");
+});
+
+favDialog.addEventListener("close", (e) => {
+    console.log(
+    favDialog.returnValue === "default"
+      ? "No return value."
+      : `ReturnValue: ${favDialog.returnValue}`); // Have to check for "default" rather than empty string
+});
+
+
+function Book(title, page_count, author, is_read) {
     this.title = title;
     this.page_count = page_count;
     this.author = author;
     this.is_read = is_read;
 
-    this.info = function(){
+    this.info = function () {
         return this.title + ", by " + this.author + " with " + this.page_count + " pages and has been read: " + this.is_read;
     }
 }
 
-function addBookToLibrary(){
+function addBookToLibrary() {
 
 }
 
-function displayBooks(){
+function displayBooks() {
     const table = document.createElement('table');
 
-     const headerRow = document.createElement("tr");
-        const headers = ["Title", "Page Count", "Author", "Has Been Read"];
-        
-        headers.forEach(headerText => {
-            const th = document.createElement("th");
-            th.textContent = headerText;
-            headerRow.appendChild(th);
-        });
-        table.appendChild(headerRow);
+    const headerRow = document.createElement("tr");
+    const headers = ["Title", "Page Count", "Author", "Has Been Read"];
+
+    headers.forEach(headerText => {
+        const th = document.createElement("th");
+        th.textContent = headerText;
+        headerRow.appendChild(th);
+    });
+    table.appendChild(headerRow);
 
     myLibrary.forEach(book => {
         const row = document.createElement('tr');
         let title = document.createElement('td');
         title.textContent = book.title;
         row.appendChild(title);
+        
+        const pageCountCell = document.createElement("td");
+        pageCountCell.textContent = book.page_count;
+        row.appendChild(pageCountCell);
+
+        const authorCell = document.createElement("td");
+        authorCell.textContent = book.author;
+        row.appendChild(authorCell);
+
+        const isReadCell = document.createElement("td");
+        isReadCell.textContent = book.is_read ? "Yes" : "No";
+        row.appendChild(isReadCell);
+
         table.appendChild(row);
     })
-        libraryTableRow.appendChild(table); 
+    libraryTableRow.appendChild(table);
 }
